045245619c80f284f12897fe2a8f98cec6a62afe
Cassidy2024 <157302466+Cassidy2024@users.noreply.github.com>
2024-08-23 19:11:07 +0400

    Code solution

--- index.js
+++ index.js
@@ -6,26 +6,36 @@
  */
 
 // Given Parameters
-const vel = 10000; // velocity (km/h)
-const acc = 3; // acceleration (m/s^2)
+const velKmPerH = 10000; // velocity (km/h)
+const acceleration = 3; // acceleration (m/s^2)
 const time = 3600; // seconds (1 hour)
-const d = 0; // distance (km)
+const distance = 0; // distance (km)
 const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5; // fuel burn rate (kg/s)
+const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
 
+const newVelKmPerS = velKmPerH / 3600;
+const newDistance = distance + (newVelKmPerS*time); //calcultes new distance
+//console.log(newDistance);
 
-const d2 = d + (vel*time) //calcultes new distance
-const rf = fbr*time //calculates remaining fuel
-const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
+const remainingFuel = fuel - (fuelBurnRate*time); //calculates remaining fuel
+//console.log(remainingFuel);
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (vel, acc, time) => { 
-  return vel + (acc*time)
-}
+let calcNewVel = (velKmPerH, acceleration, time) => { 
+
+  if (typeof velKmPerH !== 'number' || typeof acceleration !== 'number' || typeof time !== 'number') {
+    throw new Error('Ensure all arguments are numbers');
+  }
+  
+  return velKmPerH + (acceleration*time* 3.6); // "3.6" converts m/s to km/h
+};
+
+const vel2 = calcNewVel(velKmPerH, acceleration, time); //calculates new velocity based on acceleration
+//console.log(vel2);
 
 console.log(`Corrected New Velocity: ${vel2} km/h`);
-console.log(`Corrected New Distance: ${d2} km`);
-console.log(`Corrected Remaining Fuel: ${rf} kg`);
+console.log(`Corrected New Distance: ${newDistance} km`);
+console.log(`Corrected Remaining Fuel: ${remainingFuel} kg`);
 
 
 
